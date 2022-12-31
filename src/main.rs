796a9d0afb15f3b108eac46b4a7dbf42ba113aa0
Martinskatvedt <martinskatvedt@gmail.com>
2022-12-31 02:18:19 +0100

    imported gl code from gloom-rs to create scalable window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,127 @@
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::ControlFlow;
+use std::sync::{Arc, Mutex, RwLock};
+use std::thread;
+
+// initial window size
+const INITIAL_SCREEN_W: u32 = 800;
+const INITIAL_SCREEN_H: u32 = 600;
+
 fn main() {
-    println!("Hello, world!");
+    let el = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new()
+        .with_title("studios-systems")
+        .with_resizable(true)
+        .with_inner_size(glutin::dpi::LogicalSize::new(
+            INITIAL_SCREEN_W,
+            INITIAL_SCREEN_H,
+        ));
+    let cb = glutin::ContextBuilder::new().with_vsync(true);
+    let windowed_context = cb.build_windowed(wb, &el).unwrap();
+
+    let arc_window_size = Arc::new(Mutex::new((INITIAL_SCREEN_W, INITIAL_SCREEN_H, false)));
+    // Make a reference of this tuple to send to the render thread
+    let window_size = Arc::clone(&arc_window_size);
+
+    // Spawn a separate thread for rendering, so event handling doesn't block rendering
+    let render_thread = thread::spawn(move || {
+        // Acquire the OpenGL Context and load the function pointers.
+        // This has to be done inside of the rendering thread, because
+        // an active OpenGL context cannot safely traverse a thread boundary
+        let context = unsafe {
+            let c = windowed_context.make_current().unwrap();
+            gl::load_with(|symbol| c.get_proc_address(symbol) as *const _);
+            c
+        };
+
+        // Set up openGL
+        unsafe {
+            gl::Enable(gl::DEPTH_TEST);
+            gl::DepthFunc(gl::LESS);
+            gl::Enable(gl::CULL_FACE);
+            gl::Disable(gl::MULTISAMPLE);
+            gl::Enable(gl::BLEND);
+            gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+            gl::Enable(gl::DEBUG_OUTPUT_SYNCHRONOUS);
+        }
+
+        let first_frame_time = std::time::Instant::now();
+        let mut prevous_frame_time = first_frame_time;
+        loop {
+            // Compute time passed since the previous frame and since the start of the program
+            let now = std::time::Instant::now();
+            let _elapsed = now.duration_since(first_frame_time).as_secs_f32();
+            let _delta_time = now.duration_since(prevous_frame_time).as_secs_f32();
+            prevous_frame_time = now;
+
+            // Handle resize events
+            if let Ok(mut new_size) = window_size.lock() {
+                if new_size.2 {
+                    context.resize(glutin::dpi::PhysicalSize::new(new_size.0, new_size.1));
+                    (*new_size).2 = false;
+                    println!("Resized");
+                    unsafe {
+                        gl::Viewport(0, 0, new_size.0 as i32, new_size.1 as i32);
+                    }
+                }
+            }
+
+            unsafe {
+                // Clear the color and depth buffers
+                gl::ClearColor(0.035, 0.046, 0.078, 1.0); // night sky, full opacity
+                gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+
+                // == // Issue the necessary gl:: commands to draw your scene here
+            }
+
+            // Display the new color buffer on the display
+            context.swap_buffers().unwrap(); // we use "double buffering" to avoid artifacts
+        }
+    });
+
+    // Keep track of the health of the rendering thread
+    let render_thread_healthy = Arc::new(RwLock::new(true));
+    let render_thread_watchdog = Arc::clone(&render_thread_healthy);
+    thread::spawn(move || {
+        if !render_thread.join().is_ok() {
+            if let Ok(mut health) = render_thread_watchdog.write() {
+                println!("Render thread panicked!");
+                *health = false;
+            }
+        }
+    });
+
+    // Start the event loop -- This is where window events are initially handled
+    el.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        // Terminate program if render thread panics
+        if let Ok(health) = render_thread_healthy.read() {
+            if *health == false {
+                *control_flow = ControlFlow::Exit;
+            }
+        }
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::Resized(physical_size),
+                ..
+            } => {
+                println!(
+                    "New window size! width: {}, height: {}",
+                    physical_size.width, physical_size.height
+                );
+                if let Ok(mut new_size) = arc_window_size.lock() {
+                    *new_size = (physical_size.width, physical_size.height, true);
+                }
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            _ => {}
+        }
+    });
 }
