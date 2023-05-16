750ef8f2ce4af08534f43a51d1fe6a9597e39705
Martin Skatvedt <martinskatvedt@gmail.com>
2023-05-16 21:12:07 +0200

    sphere with light

--- shaders/simple.frag
+++ shaders/simple.frag
@@ -1,9 +1,10 @@
 #version 450 core
 
 out vec4 color;
-layout(location=2) in vec4 inColor;
+
+layout(location=3) in vec4 inColor;
 
 void main()
 {
-  color = inColor;
+    color=inColor;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate nalgebra_glm as glm;
+use lerp::Lerp;
 use std::ptr;
+use std::rc::Rc;
 use std::sync::{Arc, Mutex, RwLock};
 use std::thread;
 
@@ -16,41 +18,298 @@ use glutin::event_loop::ControlFlow;
 const INITIAL_SCREEN_W: u32 = 800;
 const INITIAL_SCREEN_H: u32 = 600;
 
+const WHITE: [f32; 4] = [0.1, 0.1, 0.6, 1.0];
+
 pub mod shader;
 pub mod utils;
 
 #[derive(Copy, Clone)]
-struct Vertex {
-    position: [f32; 3],
+pub struct Vertex {
+    position: glm::Vec3,
     color: [f32; 4],
 }
 
-impl Vertex {
-    fn flatten(&self) -> Vec<f32> {
-        let mut vec = Vec::new();
-        vec.extend_from_slice(&self.position);
-        vec.extend_from_slice(&self.color);
-        vec
-    }
+#[derive(Copy, Clone)]
+pub struct Triangle {
+    pub a: usize,
+    pub b: usize,
+    pub c: usize,
 }
 
-struct Triangle {
-    vertices: [Vertex; 3],
+pub struct Shape {
+    pub vertices: Vec<f32>,
+    pub colors: Vec<f32>,
+    pub indices: Vec<u32>,
+    pub normals: Vec<f32>,
+    pub index_count: i32,
 }
 
 impl Triangle {
-    fn new(v_1: Vertex, v_2: Vertex, v_3: Vertex) -> Triangle {
-        Triangle {
-            vertices: [v_1, v_2, v_3],
+    fn new(a: usize, b: usize, c: usize) -> Triangle {
+        Triangle { a, b, c }
+    }
+}
+struct Polyhedron {
+    vertices: Vec<Vertex>,
+    triangles: Vec<Triangle>,
+    shape: Shape,
+}
+
+impl Polyhedron {
+    fn regular_isocahedron() -> Polyhedron {
+        let phi = (1.0 + (5.0 as f32).sqrt()) / 2.0;
+        let mut regular_iscocahedron = Polyhedron {
+            vertices: vec![
+                Vertex {
+                    position: glm::vec3(-1.0, phi, 0.0),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(1.0, phi, 0.0),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(-1.0, -phi, 0.0),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(1.0, -phi, 0.0),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(0.0, -1.0, phi),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(0.0, 1.0, phi),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(0.0, -1.0, -phi),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(0.0, 1.0, -phi),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(phi, 0.0, -1.0),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(phi, 0.0, 1.0),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(-phi, 0.0, -1.0),
+                    color: WHITE,
+                },
+                Vertex {
+                    position: glm::vec3(-phi, 0.0, 1.0),
+                    color: WHITE,
+                },
+            ],
+            triangles: vec![
+                Triangle::new(0, 11, 5),
+                Triangle::new(0, 5, 1),
+                Triangle::new(0, 1, 7),
+                Triangle::new(0, 7, 10),
+                Triangle::new(0, 10, 11),
+                Triangle::new(1, 5, 9),
+                Triangle::new(5, 11, 4),
+                Triangle::new(11, 10, 2),
+                Triangle::new(10, 7, 6),
+                Triangle::new(7, 1, 8),
+                Triangle::new(3, 9, 4),
+                Triangle::new(3, 4, 2),
+                Triangle::new(3, 2, 6),
+                Triangle::new(3, 6, 8),
+                Triangle::new(3, 8, 9),
+                Triangle::new(4, 9, 5),
+                Triangle::new(2, 4, 11),
+                Triangle::new(6, 2, 10),
+                Triangle::new(8, 6, 7),
+                Triangle::new(9, 8, 1),
+            ],
+            shape: Shape {
+                vertices: Vec::new(),
+                indices: Vec::new(),
+                normals: Vec::new(),
+                colors: Vec::new(),
+                index_count: 0,
+            },
+        };
+
+        regular_iscocahedron.subdivide(2.0, 4);
+        regular_iscocahedron.generate_render_shape();
+
+        regular_iscocahedron
+    }
+
+    fn add_vertex(&mut self, vertex: Vertex) -> usize {
+        self.vertices.push(vertex);
+        self.vertices.len() - 1
+    }
+
+    fn generate_render_shape(&mut self) {
+        self.shape = Shape {
+            vertices: self.flatten_vertices(),
+            indices: self.flatten_cells(),
+            normals: self.get_vertex_normals(),
+            colors: self.flatten_colors(),
+            index_count: (self.triangles.len() * 3) as i32,
+        };
+    }
+
+    fn flatten_vertices(&self) -> Vec<f32> {
+        let mut vec = Vec::new();
+        for vertex in &self.vertices {
+            vec.extend(&vertex.position);
+        }
+        vec
+    }
+
+    fn flatten_colors(&self) -> Vec<f32> {
+        let mut vec = Vec::new();
+        for vertex in &self.vertices {
+            vec.extend(&vertex.color);
+        }
+        vec
+    }
+
+    fn flatten_cells(&self) -> Vec<u32> {
+        let mut vec = Vec::new();
+        for cell in &self.triangles {
+            vec.extend_from_slice(&[cell.a as u32, cell.b as u32, cell.c as u32]);
+        }
+        vec
+    }
+
+    fn get_triangle_normals(&mut self) -> Vec<f32> {
+        let origin = glm::vec3(0.0, 0.0, 0.0);
+        let mut vec: Vec<f32> = Vec::new();
+        for cell in &self.triangles {
+            let a = self.vertices[cell.a].position;
+            let b = self.vertices[cell.b].position;
+            let c = self.vertices[cell.c].position;
+
+            let ab = b - a;
+            let ac = c - a;
+            let mut normal = glm::cross(&ab, &ac);
+
+            let dist = b - origin;
+            if (normal.dot(&dist)) < 0.0 {
+                normal *= -1.0;
+            }
+
+            normal = glm::normalize(&normal);
+            vec.extend(&normal);
+        }
+        vec
+    }
+
+    fn get_vertex_normals(&mut self) -> Vec<f32> {
+        let mut vec: Vec<f32> = Vec::new();
+        for vertex_index in 0..self.vertices.len() {
+            let mut normal = glm::vec3(0.0, 0.0, 0.0);
+            for cell in &self.triangles {
+                if cell.a == vertex_index || cell.b == vertex_index || cell.c == vertex_index {
+                    let a = self.vertices[cell.a].position;
+                    let b = self.vertices[cell.b].position;
+                    let c = self.vertices[cell.c].position;
+
+                    let ab = b - a;
+                    let ac = c - a;
+                    let mut triangle_normal = glm::cross(&ab, &ac);
+
+                    triangle_normal = glm::normalize(&triangle_normal);
+                    normal += triangle_normal;
+                }
+            }
+            normal = glm::normalize(&normal);
+            vec.extend(&normal);
+        }
+
+        vec
+    }
+
+    fn subdivide(&mut self, radius: f32, detail: u32) {
+        let triangle_copy = self.triangles.clone();
+        for triangle in triangle_copy {
+            let a = self.vertices[triangle.a].position;
+            let b = self.vertices[triangle.b].position;
+            let c = self.vertices[triangle.c].position;
+            self.subdivide_triangle(a, b, c, radius, detail);
         }
     }
 
-    fn flatten(&self) -> Vec<f32> {
-        let mut vec = Vec::new();
-        for vertex in &self.vertices {
-            vec.extend_from_slice(&vertex.flatten());
+    fn subdivide_triangle(
+        &mut self,
+        a: glm::Vec3,
+        b: glm::Vec3,
+        c: glm::Vec3,
+        radius: f32,
+        detail: u32,
+    ) {
+        let cols = 2usize.pow(detail);
+        let mut new_vertices: Vec<Vec<glm::Vec3>> = vec![];
+
+        for i in 0..=cols {
+            new_vertices.push(vec![]);
+            let aj = a.clone().lerp(c, i as f32 / cols as f32);
+            let bj = b.clone().lerp(c, i as f32 / cols as f32);
+            let rows = cols - i;
+
+            for j in 0..=rows {
+                if j == 0 && i == cols {
+                    new_vertices[i].push(aj.normalize() * radius);
+                } else {
+                    new_vertices[i]
+                        .push(aj.clone().lerp(bj, j as f32 / rows as f32).normalize() * radius);
+                }
+            }
         }
-        vec
+
+        for i in 0..cols {
+            for j in 0..2 * (cols - i) - 1 {
+                let k = j / 2;
+
+                let mut triangle = Triangle { a: 0, b: 0, c: 0 };
+                if j % 2 == 0 {
+                    triangle.a = self.add_vertex(Vertex {
+                        position: new_vertices[i][k + 1],
+                        color: WHITE,
+                    });
+
+                    triangle.b = self.add_vertex(Vertex {
+                        position: new_vertices[i + 1][k],
+                        color: WHITE,
+                    });
+
+                    triangle.c = self.add_vertex(Vertex {
+                        position: new_vertices[i][k],
+                        color: WHITE,
+                    });
+                } else {
+                    triangle.a = self.add_vertex(Vertex {
+                        position: new_vertices[i][k + 1],
+                        color: WHITE,
+                    });
+
+                    triangle.b = self.add_vertex(Vertex {
+                        position: new_vertices[i + 1][k + 1],
+                        color: WHITE,
+                    });
+
+                    triangle.c = self.add_vertex(Vertex {
+                        position: new_vertices[i + 1][k],
+                        color: WHITE,
+                    });
+                }
+
+                self.triangles.push(triangle);
+            }
+        }
     }
 }
 
@@ -112,6 +371,90 @@ unsafe fn create_vao(vertices: &Vec<f32>, indices: &Vec<u32>) -> u32 {
     vao_ids
 }
 
+unsafe fn create_shape_vao(shape: &Shape) -> u32 {
+    let mut vao_ids: u32 = 0;
+    gl::GenVertexArrays(1, &mut vao_ids as *mut u32);
+    gl::BindVertexArray(vao_ids);
+
+    let mut vbo_ids: u32 = 0;
+    gl::GenBuffers(1, &mut vbo_ids as *mut u32);
+    gl::BindBuffer(gl::ARRAY_BUFFER, vbo_ids);
+
+    gl::BufferData(
+        gl::ARRAY_BUFFER,
+        utils::byte_size_of_array(&shape.vertices),
+        utils::pointer_to_array(&shape.vertices),
+        gl::STATIC_DRAW,
+    );
+
+    gl::VertexAttribPointer(
+        0,
+        3,
+        gl::FLOAT,
+        gl::FALSE,
+        utils::size_of::<f32>() * 3,
+        ptr::null(),
+    );
+    gl::EnableVertexAttribArray(0);
+
+    //Color buffer
+    let mut color_vbo_ids: u32 = 1;
+    gl::GenBuffers(1, &mut color_vbo_ids as *mut u32);
+    gl::BindBuffer(gl::ARRAY_BUFFER, color_vbo_ids);
+
+    gl::BufferData(
+        gl::ARRAY_BUFFER,
+        utils::byte_size_of_array(&shape.colors),
+        utils::pointer_to_array(&shape.colors),
+        gl::STATIC_DRAW,
+    );
+
+    gl::VertexAttribPointer(
+        1,
+        4,
+        gl::FLOAT,
+        gl::FALSE,
+        utils::size_of::<f32>() * 4,
+        ptr::null(),
+    );
+    gl::EnableVertexAttribArray(1);
+
+    let mut normvec_vbo_ids: u32 = 1;
+    gl::GenBuffers(1, &mut normvec_vbo_ids as *mut u32);
+    gl::BindBuffer(gl::ARRAY_BUFFER, normvec_vbo_ids);
+
+    gl::BufferData(
+        gl::ARRAY_BUFFER,
+        utils::byte_size_of_array(&shape.normals),
+        utils::pointer_to_array(&shape.normals),
+        gl::STATIC_DRAW,
+    );
+
+    gl::VertexAttribPointer(
+        2,
+        3,
+        gl::FLOAT,
+        gl::FALSE,
+        utils::size_of::<f32>() * 3,
+        ptr::null(),
+    );
+    gl::EnableVertexAttribArray(2);
+
+    let mut ibo_ids: u32 = 0;
+    gl::GenBuffers(1, &mut ibo_ids as *mut u32);
+    gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ibo_ids);
+
+    // * Fill it with data
+    gl::BufferData(
+        gl::ELEMENT_ARRAY_BUFFER,
+        utils::byte_size_of_array(&shape.indices),
+        utils::pointer_to_array(&shape.indices),
+        gl::STATIC_DRAW,
+    );
+
+    vao_ids
+}
+
 fn main() {
     let el = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new()
@@ -129,7 +472,7 @@ fn main() {
 
     let pressed_keys = Arc::clone(&arc_pressed_keys);
 
-    // Set up shared tuple for tracking mouse movement between frames
+    // Set up shaWHITE tuple for tracking mouse movement between frames
     let arc_mouse_delta = Arc::new(Mutex::new((0f32, 0f32)));
     // Make a reference of this tuple to send to the render thread
     let mouse_delta = Arc::clone(&arc_mouse_delta);
@@ -161,44 +504,18 @@ fn main() {
             gl::Enable(gl::DEBUG_OUTPUT_SYNCHRONOUS);
         }
 
-        let white = [1.0, 1.0, 1.0, 1.0];
+        let regular_icosahedron = Polyhedron::regular_isocahedron();
 
-        let triangle_1 = Triangle::new(
-            Vertex {
-                position: [-0.5, -0.5, 0.0],
-                color: [1.0, 0.0, 0.0, 1.0],
-            },
-            Vertex {
-                position: [0.0, -0.5, 0.0],
-                color: [1.0, 0.0, 0.0, 1.0],
-            },
-            Vertex {
-                position: [-0.5, 0.0, 0.0],
-                color: [1.0, 0.0, 0.0, 1.0],
-            },
-        )
-        .flatten();
+        println!("vertices: {}", regular_icosahedron.shape.vertices.len());
+        println!("normals: {}", regular_icosahedron.shape.normals.len());
 
-        let triangle_2 = Triangle::new(
-            Vertex {
-                position: [-0.5, 0.0, 0.0],
-                color: white,
-            },
-            Vertex {
-                position: [0.0, -0.5, 0.0],
-                color: white,
-            },
-            Vertex {
-                position: [0.0, 0.0, 0.0],
-                color: white,
-            },
-        )
-        .flatten();
+        //        let triangle_vec = regular_icosahedron.flatten_vertices();
+        //
+        //        let indices = regular_icosahedron.flatten_cells();
+        //
+        // let triangle_vao = unsafe { create_vao(&triangle_vec, &indices) };
 
-        let triangle_vec: Vec<f32> = vec![triangle_1, triangle_2].concat();
-        let indices: Vec<u32> = vec![0, 1, 2, 3, 4, 5];
-
-        let triangle_vao = unsafe { create_vao(&triangle_vec, &indices) };
+        let icosahedron_vao = unsafe { create_shape_vao(&regular_icosahedron.shape) };
 
         let shader = unsafe {
             shader::ShaderBuilder::new()
@@ -208,7 +525,8 @@ fn main() {
         };
 
         unsafe {
-            gl::BindVertexArray(triangle_vao);
+            gl::BindVertexArray(icosahedron_vao);
+            //gl::BindVertexArray(triangle_vao);
             shader.activate();
         }
 
@@ -225,6 +543,11 @@ fn main() {
 
         let move_speed: f32 = 10.0;
         let cam_speed: f32 = 100.0;
+
+        let mut light = Vertex {
+            position: glm::vec3(1.0, 0.0, 1.0),
+            color: [1.0, 1.0, 1.0, 1.0],
+        };
 
         loop {
             // Compute time passed since the previous frame and since the start of the program
@@ -302,6 +625,9 @@ fn main() {
                 // == // Optionally access the acumulated mouse movement between
                 // == // frames here with `delta.0` and `delta.1`
 
+                // yaw += delta.0 as f32 * 0.1;
+                //pitch += delta.1 as f32 * 0.1;
+
                 *delta = (0.0, 0.0); // reset when done
             }
 
@@ -326,15 +652,21 @@ fn main() {
                 transformation_matrix =
                     projection_matrix * view_matrix * model_matrix * transformation_matrix;
 
-                gl::UniformMatrix4fv(4, 1, gl::TRUE, transformation_matrix.as_ptr());
+                light.position += glm::vec3(0.001, 0.0, 0.0);
+                if (light.position.x > 1.0) {
+                    light.position.x = -1.0;
+                }
 
+                gl::UniformMatrix4fv(4, 1, gl::TRUE, transformation_matrix.as_ptr());
+                gl::Uniform4f(5, 1.0, 1.0, 1.0, 1.0);
+                gl::Uniform3f(6, light.position.x, light.position.y, light.position.z);
                 // Clear the color and depth buffers
                 gl::ClearColor(0.035, 0.046, 0.078, 1.0); // night sky, full opacity
                 gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
                 gl::DrawElements(
                     gl::TRIANGLES,
-                    (triangle_vec.len() / 7) as i32,
+                    regular_icosahedron.shape.index_count,
                     gl::UNSIGNED_INT,
                     ptr::null(),
                 );
